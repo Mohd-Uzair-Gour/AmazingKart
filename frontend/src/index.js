4493bbbc00807c75889301b2bc9d619779c82888
Mohd Uzair <muzair6302@gmail.com>
2024-11-20 11:34:17 +0530

    Fixed dependency issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,21 +8,16 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>  
+    </React.StrictMode>
   </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
